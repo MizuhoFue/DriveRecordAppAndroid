4638e60d6abedd31beb216d7e948e09191413efe
MizuhoFue <Gentiana461008@gmail.com>
2020-10-26 13:13:42 +0900

    パッケージ名driveandroidに修正

--- app/src/androidTest/java/com/example/driveandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/driveandroid/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.drivefueki1012", appContext.packageName)
+        assertEquals("com.example.driveandroid", appContext.packageName)
     }
 }
