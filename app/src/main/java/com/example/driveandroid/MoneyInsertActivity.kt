240beff06b4bfab55069c572627fa46b71ad27b2
MizuhoFue <Gentiana461008@gmail.com>
2020-11-04 15:48:20 +0900

    MoneyInsertのレイアウト調整、前回のレビュー指摘箇所修正、ParagraphDBHelper仮作成

--- app/src/main/java/com/example/driveandroid/MoneyInsertActivity.kt
+++ app/src/main/java/com/example/driveandroid/MoneyInsertActivity.kt
@@ -1,134 +1,134 @@
-package com.example.driveandroid
 /*FolderCreate、FolderDetailから遷移
 *画面名：MoneyInsertActivity 金額入力画面　使用した金額や使用用途の入力・登録を行う　ひとまずMainActivityを参考につくって部品化　
 *
 * 整理：入力された値を変数に入れてSQL実行　一度に登録できるのは1項目　負担者参照する場合folderidで指定する感じになりそう
-* 　　　遷移前のActivityから送ってもらう必要あり？　ダイアログは後で　ひとまずDB接続を終わらせる　
-*
+* 　　　遷移前のActivityから送ってもらう必要あり？　レイアウトはひとまず終わり遷移をひとまずやるべき
 *
 *遷移先：FolderList,FolderDetail ダイアログで選択、遷移する
 *ボタンメモ：入力完了：id:inputComp　
 *
-* やること:ダイアログ遷移を入れる　レイアウト（円だけそのまま）、データベース接続、　とりあえず選択された値を変数に入れられるようにするのが先？
+* やること:ダイアログ遷移を入れる→入力完了を押して遷移先決めたらinsert呼び出し、登録して遷移、データベース接続、　とりあえず選択された値を変数に入れられるようにするのが先？
 * Numberの値変数に入れるのはすぐできそうな感じ
 * */
+package com.example.driveandroid
 
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.Spinner
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_money_insert.*
 
 class MoneyInsertActivity : AppCompatActivity() {
-        //負担者スピナーSpinner用の仮の値をひとまず配列hutannsyaに入れておく　実装はデータベースから引っ張ってきたものを配列にいれる
-        private val hutannsya=arrayOf("神田太郎","シム太郎","秋葉三郎","新橋花子","渋谷さくら","千代田葉子")
-    //表示を担っているイメージ
+    //負担者スピナーSpinner用の仮の値をひとまず配列payerSpinnerに入れておく　実装はデータベースから引っ張ってきたものを配列にいれる
+    //private var <> Array<payerSpinner>.onItemSelectedListener: AdapterView.OnItemSelectedListener
+    //    get() {}
+    //    set() {}
+    //負担者スピナーの配列　アダプター使用　仮データを配列に入れる
+    val payerList = arrayListOf<String>("神田太郎", "シム太郎", "秋葉三郎", "新橋花子", "渋谷さくら", "千代田葉子")
+
+    //表示
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_money_insert)
+        //項目スピナー設定 ダイアログ表示、選択項目Spinnerスペースへの表示ができない↓
+        paragraphSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //選択された項目名を変数paragraphに代入
+                val spinner1 = parent as? Spinner
+                val paragraph = spinner1?.selectedItem as? String
 
-        //項目名はSpinner、ダイアログバージョンで選ぶようにする　部品を変数に割り当てる
-        val koumokuSpinner=findViewById<Spinner>(R.id.koumokuSpinner)
+            }
 
-        //選択肢を表示するためにはadapterが必要  変数用意  moneyinsert_spinnerのkoumokuSpinnerタグの文字列を使用、simple_spinner_itemらへんは用意されてる引数？
-        val adapterKoumoku= ArrayAdapter.createFromResource(this,R.array.koumokuSpinner,android.R.layout.simple_spinner_item)
-
-        //メソッドを使ってやってみよう〜（？）　
-        adapterKoumoku.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        //これはなんでしょうね　言語化できない
-        koumokuSpinner.adapter=adapterKoumoku    //ダイアログ表示まで確認おっけ
-
-        //負担者用Spinner設定　こちらはデータベースからもってくるから先にデータベースで仮データを登録する必要がありそう　
-        val hutannsyaSpinner=findViewById<Spinner>(R.id.hutannsyaSpinner)
-
-        //adapter生成　Spinnerごとにつくるっぽい ひとまず用意した配列hutannsyaを参照
-        val adapterHutannsya=ArrayAdapter(this, android.R.layout.simple_spinner_item, hutannsya)
-
-        //メソッドを使ってやってみよう〜（？）　
-        adapterHutannsya.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        //これはなんでしょうね
-        hutannsyaSpinner.adapter=adapterHutannsya //ダイアログ表示まで確認おっけ　　レイアウト崩れとるがあとでいいか・・・
-
-        //入力完了ボタン（inputComp）を遷移用ボタンとして設定
-        val inputButton: Button=findViewById<Button>(R.id.inputComp)
-
-        inputButton.setOnClickListener{//押したら
-
-            //Intent作成 金額入力からフォルダー詳細に遷移
-            val intent = Intent(this@MoneyInsertActivity,
-                FolderDetail::class.java)
-            startActivity(intent)
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
 
-        //カメラボタン設定
-        val camerabutton: Button=findViewById<Button>(R.id.camera)
-        //クリックするとCameraActivityに遷移
-        camerabutton.setOnClickListener{
+        //アダプターに負担者配列リストを設定
+        val Adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, payerList)
+        //Spinnerにアダプター設定
+        payerSpinner.adapter = Adapter
 
-            val intentcamera= Intent(this@MoneyInsertActivity,CameraActivity::class.java)
-            startActivity(intentcamera)
+        //プルダウンをクリックした時ダイアログを表示
+        payerSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //選択されたアイテムを負担者payerに代入
+                val spinner2 = parent as? Spinner
+                var payer = spinner2?.selectedItem as? String
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
 
+        //カメラボタンをクリックするとCameraActivityに遷移
+        camera.setOnClickListener {
+            val intentCamera = Intent(this@MoneyInsertActivity, CameraActivity::class.java)
+            startActivity(intentCamera)
+        }
 
+        //入力完了を押した際にダイアログを表示
+        inputComp.setOnClickListener {//押した
 
+            //insert処理を入れる　ここでparagraph,payerを使うが関数自体はParagrarphDBHelperに入れる？
+
+            //ダイアログのタイトル、メッセージ、各ボタンの処理を設定
+            AlertDialog.Builder(this).setTitle("入力完了").setMessage("どちらに移動しますか？")
+                .setPositiveButton(
+                    "詳細",
+                    DialogInterface.OnClickListener { dialog, which ->      //なぜ波線ついてる？
+                        val intent = Intent(
+                            this@MoneyInsertActivity, FolderDetailActivity::class.java
+                        )
+                        startActivity(intent)
+                    })
+                .setNegativeButton("ホーム", DialogInterface.OnClickListener { dialog, which ->
+                    val intent = Intent(this@MoneyInsertActivity, FolderListActivity::class.java)
+                    startActivity(intent)   //こちらはfinish()処理を入れたほうがよいかも
+                })
+                .show() //またはcreate() ?
+        }
     }
-
-    //DB用変数
-    private val dbName: String = "drivedb"
-    private val tableName2: String = "ParagraphInfo"
-    private val dbVersion: Int=1 //これがよくわからない
-
-    //各入力項目変数用意
-    val paraName: String="Spinnerで選択された項目名"
-    val paraCost:Int=200 //入力された値段　
-    val repayer="選択された負担者の値"
-
-
-
-
-
-    //各項目入力、変数に入れる
-
-
-
-
-
-
-
-
-
-
-
-    //ボタンを押したら入力チェック
-
-    fun checkInsert(view: View){
-
-        //チェック事項は　各変数の空白、不整値　種類は？　 模擬開発演習コードなどを参考
-
-
-
-    }
-
-    //問題なければinsert文呼び出し　ここに定義というかinsertするのはこの画面だからひとまずベタ書きする
-    fun ParaInsert(){
-
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-
 }
+
+////////////////////////////メモ↓//////////////////////////////////////////////////////////////////////////////////
+//    //DB用変数
+//    private val dbName: String = "drivedb"
+//    private val tableName2: String = "ParagraphInfo"
+//    private val dbVersion: Int = 1 //これがよくわからない
+//
+//    //各入力項目変数用意
+//    val paraName: String = "Spinnerで選択された項目名"
+//    val paraCost: Int = 200 //入力された値段　
+//    val repayer = "選択された負担者の値"
+//
+//
+//    //各項目入力、変数に入れる
+//   //ボタンを押したら入力チェック
+//
+//    fun checkInsert(view: View) {
+//
+//        //チェック事項は　各変数の空白、不整値　種類は？　 模擬開発演習コードなどを参考
+//
+//
+//    }
+//
+//    //問題なければinsert文呼び出し　
+//    fun ParaInsert() {
+// }
+//
+
---
+++ app/src/main/java/com/example/driveandroid/ParagraphDBHelper.kt
@@ -0,0 +1,9 @@
+/*
+* このファイルでParagraphInfoテーブル作成、insert、delete関数を設定
+*
+* * 作成者：笛木
+* 更新日：20201104
+状況：コンパイルエラーは見られずアプリの実行はできる状態、何度か実行しているがDeviceFile Explorerのdataにdbファイルが現れない
+* */
+package com.example.driveandroid
+
