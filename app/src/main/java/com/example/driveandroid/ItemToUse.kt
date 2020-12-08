430c8f16f584db5648ec7e2490f447d16ba39afc
星野未来 <miku.hsn.39@gmail.com>
2020-12-08 11:49:07 +0900

    valに変更

--- app/src/main/java/com/example/driveandroid/ItemToUse.kt
+++ app/src/main/java/com/example/driveandroid/ItemToUse.kt
@@ -2,9 +2,9 @@ package com.example.driveandroid
 
 data class ItemToUse(
     //配列初期化
-    var folderid: Int, //id
-    var paraNum: Int, //paraNum
-    var paraNames: String, //使用項目名
-    var paraCosts: String, //金額表示
-    var payers: String //負担者表示
+    val folderid: Int, //id
+    val paraNum: Int, //paraNum
+    val paraNames: String, //使用項目名
+    val paraCosts: String, //金額表示
+    val payers: String //負担者表示
 )
