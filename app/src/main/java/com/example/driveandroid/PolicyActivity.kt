275cbfbedaeb1b2e2070ce8b4ff726432d74df7a
星野未来 <miku.hsn.39@gmail.com>
2021-01-05 16:51:24 +0900

    利用規約のデザイン

--- app/src/main/java/com/example/driveandroid/PolicyActivity.kt
+++ app/src/main/java/com/example/driveandroid/PolicyActivity.kt
@@ -1,6 +1,9 @@
 package com.example.driveandroid
 
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_policy.*
 
@@ -15,5 +18,15 @@ class PolicyActivity : AppCompatActivity() {
         drive_toolbar.setNavigationOnClickListener {
             finish()    //finishしてサポートへ
         }
+        //リンクからブラウザにとぶ処理
+        val policy: TextView = this.policy_8_url
+        // policyをclickableにする
+        val mMethod = LinkMovementMethod.getInstance()
+        policy.movementMethod = mMethod
+        val url = "https://www.cimtech.co.jp/privacy/"
+        // urlをhtmlを使ってリンクテキストに埋め込む
+        val link: CharSequence =
+            Html.fromHtml("<a href=\"$url\">https://www.cimtech.co.jp/privacy/</a>")
+        policy.text = link
     }
 }
